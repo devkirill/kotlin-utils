be0480100b4ee6c40c86ab455425e87767f27c5a
DevKirill <4599661+devkirill@users.noreply.github.com>
2023-11-25 02:34:41 +0500

    upd version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.9.10"
     application
+    `maven-publish`
 }
 
 group = "com.github.devkirill"
@@ -10,13 +11,26 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-//    maven(url = "https://jitpack.io")
+    maven(url = "https://jitpack.io")
 }
 
 dependencies {
+    implementation(kotlin("stdlib-jdk8"))
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
     testImplementation(kotlin("test"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.github.devkirill"
+            artifactId = "kotlin-utils"
+            version = "1.0-SNAPSHOT"
+
+            from(components["java"])
+        }
+    }
 }
 
 tasks.test {
